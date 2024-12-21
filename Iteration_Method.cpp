bd2128983d6744d32a068156136685bf8fde0dfe
jija222 <bragihin@mail.ru>
2024-12-22 01:04:19 +0500

    code

---
+++ Iteration_Method.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+const float exactNum = 3.2300;
+
+float newNum(float prevX) {
+	return ((4 * log(prevX) + 5) / 3);
+}
+
+int main() {
+	for (float i = 2.0; ; i = newNum(i)) {
+		cout << setprecision(5) << i << endl;
+		if (i == newNum(i))
+			break;
+	} 
+	return 0;
+}
+
